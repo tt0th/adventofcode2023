15f5f4aa9d228b432bc755a53cd330aba77175d8
Tibor Toth <tibor-toth@telekom.com>
2023-12-08 17:24:52 +0100

    feat: add day8 part2

--- src/day8/day8.go
+++ src/day8/day8.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/samber/lo"
+	lop "github.com/samber/lo/parallel"
+	"math"
 	"os"
 	"strings"
 )
@@ -16,11 +19,23 @@ type Node struct {
 func main() {
 	//path := "src/day8/test-input-1.txt"
 	//path := "src/day8/test-input-2.txt"
+	//path := "src/day8/test-input-3.txt"
 	path := "src/day8/input.txt"
 	var nodes, directions = parseInput(path)
 
 	requiredSteps := getStepsRequiredToZZZ(directions, nodes)
 	fmt.Printf("Steps to ZZZ: %d\n", requiredSteps)
+
+	currentNodes := lo.Filter(lo.Keys(nodes), func(nodeId string, _ int) bool {
+		return []rune(nodeId)[2] == 'A'
+	})
+	loops := lop.Map(currentNodes, func(nodeId string, _ int) Loop {
+		return getLoop(directions, nodes, nodeId)
+	})
+	printObject(loops)
+	printObject(lo.Reduce(loops, func(agg int, loop Loop, _ int) int {
+		return lcm(agg, loop.length)
+	}, 1))
 }
 
 func getStepsRequiredToZZZ(directions []rune, nodes map[string]Node) int {
@@ -34,6 +49,49 @@ func getStepsRequiredToZZZ(directions []rune, nodes map[string]Node) int {
 		}
 	}
 	return i
+}
+
+type Step struct {
+	index          int
+	directionIndex int
+	node           string
+}
+type Loop struct {
+	start           int
+	length          int
+	solutionIndexes []int
+}
+
+func getLoop(directions []rune, nodes map[string]Node, startNode string) Loop {
+	currentNode := startNode
+	var solutionIndexes []int
+	var steps []Step
+	for i := 0; true; i++ {
+		step, _, found := lo.FindIndexOf(steps, func(step Step) bool {
+			return step.directionIndex == i%len(directions) && step.node == currentNode
+		})
+		if found {
+			return Loop{
+				start:           step.index,
+				length:          i - step.index,
+				solutionIndexes: solutionIndexes,
+			}
+		}
+		if []rune(currentNode)[2] == 'Z' {
+			solutionIndexes = append(solutionIndexes, i)
+		}
+		steps = append(steps, Step{index: i, directionIndex: i % len(directions), node: currentNode})
+		if directions[i%len(directions)] == 'L' {
+			currentNode = nodes[currentNode].left
+		} else {
+			currentNode = nodes[currentNode].right
+		}
+	}
+	return Loop{}
+}
+
+func printObject(object interface{}) {
+	fmt.Printf("%v\n", object)
 }
 
 func parseInput(path string) (map[string]Node, []rune) {
@@ -67,4 +125,15 @@ func parseInput(path string) (map[string]Node, []rune) {
 	}
 
 	return nodes, directions
+}
+
+// lcm and gcd from https://github.com/TheAlgorithms/Go
+func lcm(a, b int) int {
+	return int(math.Abs(float64(a*b)) / float64(gcd(a, b)))
+}
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
 }
