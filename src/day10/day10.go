4d32f88b2cc02b1fd056ba1823d7b8f670a895f4
Tibor Toth <tibor-toth@telekom.com>
2023-12-10 17:03:35 +0100

    feat: add day10 part2

--- src/day10/day10.go
+++ src/day10/day10.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/samber/lo"
+	"github.com/tt0th/adventofcode2023/src/utils"
 	"os"
 )
 
@@ -43,26 +45,207 @@ func (maze Maze) getWidth() int {
 
 func main() {
 	//path := "src/day10/test-input-1.txt"
+	//path := "src/day10/test-input-2.txt"
+	//path := "src/day10/test-input-3.txt"
+	//path := "src/day10/test-input-4.txt"
 	path := "src/day10/input.txt"
 	var maze = parseInput(path)
 
-	var stepsToFarthest int
-	loop := getLoop(maze)
-	stepsToFarthest = len(loop) / 2
+	loop, innerPoints := getLoop(maze)
+	innerPoints = extendInnerPoints(innerPoints, loop)
 
-	fmt.Printf("stepsToFarthest: %d\n", stepsToFarthest)
+	fmt.Printf("stepsToFarthest: %d, countOfInnerPoints: %d \n", len(loop)/2, len(innerPoints))
 }
 
-func getLoop(maze Maze) []C {
+func getLoop(maze Maze) ([]C, []C) {
 	var route []C
+	var pointsToTheRight, pointsToTheLeft []C
+	turns := make(map[string]int)
 	position := getStartPosition(maze)
 	direction := guessStartDirection(maze, position)
 	for maze.get(position) != 'S' || len(route) == 0 {
 		route = append(route, position)
 		position = position.add(direction)
+		pointsToTheRight = append(pointsToTheRight, getPointsToTheRight(maze.get(position), direction, position)...)
+		pointsToTheLeft = append(pointsToTheLeft, getPointsToTheLeft(maze.get(position), direction, position)...)
+		turns[getTurnDirection(maze.get(position), direction)]++
 		direction = getNewDirection(maze.get(position), direction)
+		//printMaze(maze, position, turnDirection)
 	}
-	return route
+	var innerPoints []C
+	if turns["L"] > turns["R"] {
+		innerPoints = pointsToTheLeft
+	} else {
+		innerPoints = pointsToTheRight
+	}
+	innerPoints = lo.Filter(innerPoints, func(point C, _ int) bool {
+		return !lo.Contains(route, point)
+	})
+	return route, innerPoints
+}
+
+func extendInnerPoints(points []C, excludedPoints []C) []C {
+	var pointCollection []C
+	pointsToVisit := points
+
+	for len(pointsToVisit) > 0 {
+		point := pointsToVisit[0]
+		pointsToVisit = pointsToVisit[1:]
+		if lo.Contains(excludedPoints, point) {
+			continue
+		}
+		if lo.Contains(pointCollection, point) {
+			continue
+		}
+		pointCollection = append(pointCollection, point)
+		pointsToVisit = append(pointsToVisit, point.add(up), point.add(down), point.add(left), point.add(right))
+	}
+
+	return pointCollection
+}
+
+func getPointsToTheRight(pipe rune, direction C, position C) []C {
+	if pipe == '|' {
+		if direction == up {
+			return []C{position.add(right)}
+		}
+		if direction == down {
+			return []C{position.add(left)}
+		}
+	}
+	if pipe == '-' {
+		if direction == left {
+			return []C{position.add(up)}
+		}
+		if direction == right {
+			return []C{position.add(down)}
+		}
+	}
+	if pipe == 'L' {
+		if direction == left {
+			return []C{}
+		}
+		if direction == down {
+			return []C{position.add(down), position.add(left)}
+		}
+	}
+	if pipe == 'J' {
+		if direction == right {
+			return []C{position.add(down), position.add(right)}
+		}
+		if direction == down {
+			return []C{}
+		}
+	}
+	if pipe == '7' {
+		if direction == right {
+			return []C{}
+		}
+		if direction == up {
+			return []C{position.add(up), position.add(right)}
+		}
+	}
+	if pipe == 'F' {
+		if direction == left {
+			return []C{position.add(up), position.add(left)}
+		}
+		if direction == up {
+			return []C{}
+		}
+	}
+	return []C{}
+}
+
+func getPointsToTheLeft(pipe rune, direction C, position C) []C {
+	if pipe == '|' {
+		if direction == up {
+			return []C{position.add(left)}
+		}
+		if direction == down {
+			return []C{position.add(right)}
+		}
+	}
+	if pipe == '-' {
+		if direction == left {
+			return []C{position.add(down)}
+		}
+		if direction == right {
+			return []C{position.add(up)}
+		}
+	}
+	if pipe == 'L' {
+		if direction == left {
+			return []C{position.add(down), position.add(left)}
+		}
+		if direction == down {
+			return []C{}
+		}
+	}
+	if pipe == 'J' {
+		if direction == right {
+			return []C{}
+		}
+		if direction == down {
+			return []C{position.add(down), position.add(right)}
+		}
+	}
+	if pipe == '7' {
+		if direction == right {
+			return []C{position.add(up), position.add(right)}
+		}
+		if direction == up {
+			return []C{}
+		}
+	}
+	if pipe == 'F' {
+		if direction == left {
+			return []C{}
+		}
+		if direction == up {
+			return []C{position.add(up), position.add(left)}
+		}
+	}
+	return []C{}
+}
+
+func printMaze(maze Maze, position C, turnDirection string) {
+	for i := 0; i < maze.getHeight(); i++ {
+		for j := 0; j < maze.getWidth(); j++ {
+			if position.i == i && position.j == j {
+				fmt.Printf("\u2588")
+			} else {
+				fmt.Printf("%c", maze.getByIndex(i, j))
+			}
+		}
+		fmt.Printf("    ")
+		fmt.Printf(turnDirection)
+		fmt.Printf("\n")
+	}
+	utils.PrintObject(turnDirection)
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func getTurnDirection(pipe rune, lastDirection C) string {
+	var directions = map[C]map[rune]string{
+		down: {
+			'L': "L",
+			'J': "R",
+		},
+		up: {
+			'7': "L",
+			'F': "R",
+		},
+		left: {
+			'L': "R",
+			'F': "L",
+		},
+		right: {
+			'J': "L",
+			'7': "R",
+		},
+	}
+
+	return directions[lastDirection][pipe]
 }
 
 func getNewDirection(pipe rune, lastDirection C) C {
