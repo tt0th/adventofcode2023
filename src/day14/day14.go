81a186b02106989acd8c263926d3932efd77f42e
Tibor Toth <tibor-toth@telekom.com>
2023-12-14 13:41:23 +0100

    feat: add day14 part2

--- src/day14/day14.go
+++ src/day14/day14.go
@@ -14,8 +14,20 @@ func main() {
 	path := "src/day14/input.txt"
 	var platform = parseInput(path)
 
-	rollToNorth(platform)
+	sum := summarizePlatformWeightOnNorth(platform)
 
+	const spinCount = 1000000000
+	platform = parseInput(path)
+	loop := spin(platform, spinCount)
+	platform = parseInput(path)
+	spin(platform, loop.start+(spinCount-loop.start)%loop.length)
+
+	sum2 := summarizePlatformWeightOnNorth(platform)
+
+	fmt.Printf("sum: %d, sum2: %d\n", sum, sum2)
+}
+
+func summarizePlatformWeightOnNorth(platform [][]rune) int {
 	var sum int
 	for i, row := range platform {
 		for _, column := range row {
@@ -24,16 +36,110 @@ func main() {
 			}
 		}
 	}
+	return sum
+}
 
-	fmt.Printf("sum: %d\n", sum)
+type Loop struct {
+	start  int
+	length int
+}
+
+func spin(platform [][]rune, maxTimes int) Loop {
+	var platformStateHistory [][][]rune
+	for i := 0; i < maxTimes; i++ {
+		rollToNorth(platform)
+		rollToWest(platform)
+		rollToSouth(platform)
+		rollToEast(platform)
+		for historyIndex, prevPlatform := range platformStateHistory {
+			if arePlatformsEqual(platform, prevPlatform) {
+				return Loop{
+					start:  historyIndex,
+					length: i - historyIndex,
+				}
+			}
+		}
+		platformStateHistory = append(platformStateHistory, copyPlatform(platform))
+	}
+	return Loop{}
+}
+
+func arePlatformsEqual(platform [][]rune, otherPlatform [][]rune) bool {
+	for i := 0; i < len(otherPlatform); i++ {
+		for j := 0; j < len(otherPlatform[0]); j++ {
+			if platform[i][j] != otherPlatform[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+func copyPlatform(platform [][]rune) [][]rune {
+	platformCopy := make([][]rune, len(platform))
+	for i := 0; i < len(platform); i++ {
+		platformCopy[i] = make([]rune, len(platform[0]))
+		copy(platformCopy[i], platform[i])
+	}
+	return platformCopy
 }
 
 func rollToNorth(platform [][]rune) {
-	for range platform {
+	wasChanged := true
+	for wasChanged {
+		wasChanged = false
 		for i := 0; i < len(platform)-1; i++ {
 			for j := 0; j < len(platform[0]); j++ {
 				if platform[i][j] == EMPTY && platform[i+1][j] == ROCK {
+					wasChanged = true
 					platform[i+1][j] = EMPTY
+					platform[i][j] = ROCK
+				}
+			}
+		}
+	}
+}
+
+func rollToSouth(platform [][]rune) {
+	wasChanged := true
+	for wasChanged {
+		wasChanged = false
+		for i := len(platform) - 1; i > 0; i-- {
+			for j := 0; j < len(platform[0]); j++ {
+				if platform[i][j] == EMPTY && platform[i-1][j] == ROCK {
+					wasChanged = true
+					platform[i-1][j] = EMPTY
+					platform[i][j] = ROCK
+				}
+			}
+		}
+	}
+}
+
+func rollToEast(platform [][]rune) {
+	wasChanged := true
+	for wasChanged {
+		wasChanged = false
+		for j := len(platform[0]) - 1; j > 0; j-- {
+			for i := 0; i < len(platform); i++ {
+				if platform[i][j] == EMPTY && platform[i][j-1] == ROCK {
+					wasChanged = true
+					platform[i][j-1] = EMPTY
+					platform[i][j] = ROCK
+				}
+			}
+		}
+	}
+}
+
+func rollToWest(platform [][]rune) {
+	wasChanged := true
+	for wasChanged {
+		wasChanged = false
+		for j := 0; j < len(platform[0])-1; j++ {
+			for i := 0; i < len(platform); i++ {
+				if platform[i][j] == EMPTY && platform[i][j+1] == ROCK {
+					wasChanged = true
+					platform[i][j+1] = EMPTY
 					platform[i][j] = ROCK
 				}
 			}
